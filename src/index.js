f753c4bbfe759374c2148e883940d36b402350bf
boramyy <rody.kim@kakaocorp.com>
2020-10-29 20:03:12 +0900

    Add: Stomp.. 작업중 커밋!!!

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 
 const express = require("express");
-const Stomp = require('@stomp/stompjs');
-
-const app = express();
-
+const WebSocket = require('ws');
+const { Stomp } = require('@stomp/stompjs');
 
 const SERVER_PORT = 3001;
+const SOCKET_URL = "ws://localhost:15674/ws";
+
+const app = express();
+const client = Stomp.client(SOCKET_URL);
+console.log(client)
+// const client = Stomp.overWS(SOCKET_URL);
+
+const headers = {
+  login: 'user123',
+  passcode: 'pw123',
+  // additional header
+  'client-id': 'my-client-id'
+};
+
+const connectCallback = function() {
+  console.log('연결됨')
+};
+const errorCallback = function(error) {
+  // display the error's message header:
+  alert(error.headers.message);
+};
+
+client.connect(headers, connectCallback, errorCallback);
 
 app.get("/", (req, res) => {
-  res.json({
+  const returnData = {
     msg: `hello`
+  }
+  return res.status(200).json(returnData);
+});
+
+app.get("/send/:msg", (req, res) => {
+  const options = {
+    priority: 9
+  }
+  const sendData = {
+    msg: req.params.msg
+  }
+
+  client.send("/stomp/test", options, sendData);
+
+  res.json({
+    msg: `hello stomp`
   });
 });
 
